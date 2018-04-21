32fa9f954e5c2f1522184a9c27c1e25cc17c3929
Aquaerius SSL <33044201+aquaerius@users.noreply.github.com>
2018-04-21 18:00:34 +0200

    Updated models

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+	"""docstring for Profile"""
+	name = 'Model'
+	def __unicode__(self):
+		return self.name
+		
