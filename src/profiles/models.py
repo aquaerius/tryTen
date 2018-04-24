cf80682a46e42ce197c89e139a4e8f6295ed5a21
aquaerius <aquaerius@24dcaea7d349>
2018-04-24 13:23:32 +0000

    Creates a Students model in the profiles app

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -7,4 +7,24 @@ class Profile(models.Model):
 	name = 'Model'
 	def __unicode__(self):
 		return self.name
-		
+
+
+class Student(models.Model):
+    FRESHMAN = 'FR'
+    SOPHOMORE = 'SO'
+    JUNIOR = 'JR'
+    SENIOR = 'SR'
+    YEAR_IN_SCHOOL_CHOICES = (
+        (FRESHMAN, 'Freshman'),
+        (SOPHOMORE, 'Sophomore'),
+        (JUNIOR, 'Junior'),
+        (SENIOR, 'Senior'),
+        )
+    year_in_school = models.CharField(
+        max_length=2,
+        choices=YEAR_IN_SCHOOL_CHOICES,
+        default=FRESHMAN,
+        )
+
+    def is_upperclass(self):
+        return self.year_in_school in (self.JUNIOR, self.SENIOR)
