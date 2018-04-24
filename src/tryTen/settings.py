5dd5930e7e2c35ae5098834633fece85d94ce569
aquaerius <aquaerius@24dcaea7d349>
2018-04-24 07:45:13 +0000

    Added blog app

--- src/tryTen/settings.py
+++ src/tryTen/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['aquaerius.pythonanywhere.com',]
 # Application definition
 
 INSTALLED_APPS = [
+    'blog',    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
