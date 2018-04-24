da28d21ab5586f6783bad7062066eda20b78424f
Aquaerius SSL <33044201+aquaerius@users.noreply.github.com>
2018-04-24 15:30:18 +0200

    Added Student & Profile models to the admin site

--- src/profiles/admin.py
+++ src/profiles/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Student, Profile
 
-# Register your models here.
+#Register the model in the admin site
+# Use: admin.site.register(<YOUR MODEL>)
+
+admin.site.register(Student)
+admin.site.register(Profile)
