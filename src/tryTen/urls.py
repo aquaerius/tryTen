dadcfb5ea55112e520b57bfc0d66531eb1a78c3b
aquaerius <aquaerius@24dcaea7d349>
2018-04-25 09:01:02 +0000

    Updated urls.py

--- src/tryTen/urls.py
+++ src/tryTen/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
 #    path('admin/', admin.site.urls),
-    path('admin/', include(admin.urls)),
+    path('admin/', include(admin.site.urls)),
 ]
