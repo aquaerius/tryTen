afb4fe0b8b43fe4dc408c100a3e28624b2b576d8
Aquaerius SSL <33044201+aquaerius@users.noreply.github.com>
2018-04-25 13:10:07 +0200

    Update urls.py

--- src/tryTen/urls.py
+++ src/tryTen/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-import home.views
+from home import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'^$', home.views.index, name='index'),
+    path(r'^$', include(home.urls)),
 ]
