f11b47dc8d3bdff50aec37a33b1ad90c9ad0f085
Aquaerius SSL <33044201+aquaerius@users.noreply.github.com>
2018-04-25 11:25:57 +0200

    Registered home app in settings

--- src/tryTen/settings.py
+++ src/tryTen/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['aquaerius.pythonanywhere.com',]
 # Application definition
 
 INSTALLED_APPS = [
+    'home',
     'blog',    
     'django.contrib.admin',
     'django.contrib.auth',
