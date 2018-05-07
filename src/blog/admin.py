354bc25b50e1fe7afc8c0d47ebce7307f30185de
aquaerius <aquaerius@24dcaea7d349>
2018-05-07 19:15:47 +0000

    Added Comments models to the admin panel

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 #Register the Post model in the admin site
 admin.site.register(Post)
+admin.site.register(Comment)
