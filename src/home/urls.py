7029ff4e465af1aab89d0e231251879141335bdb
Aquaerius SSL <33044201+aquaerius@users.noreply.github.com>
2018-05-07 21:10:25 +0200

    Added comment Model

--- src/home/urls.py
+++ src/home/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^accounts/login/$', authViews.login, name='login'),
     url(r'^accounts/logout/$', authViews.logout, name='logout', kwargs={'next_page': '/'}),
-
 ]
