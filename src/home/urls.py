9404c509ad1c1140abce5b2ddf9c9507029c27b4
Aquaerius SSL <33044201+aquaerius@users.noreply.github.com>
2018-05-07 21:00:06 +0200

    Added logout

--- src/home/urls.py
+++ src/home/urls.py
@@ -5,5 +5,7 @@ from django.contrib.auth import views as authViews
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^accounts/login/$', authViews.login, name='login')
+    url(r'^accounts/login/$', authViews.login, name='login'),
+    url(r'^accounts/logout/$', authViews.logout, name='logout', kwargs={'next_page': '/'}),
+
 ]
