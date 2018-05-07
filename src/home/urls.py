f799b96122d331f259dfcdbc1a1c9f95fc75792f
Aquaerius SSL <33044201+aquaerius@users.noreply.github.com>
2018-05-07 17:14:59 +0200

    Update urls.py

--- src/home/urls.py
+++ src/home/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import include, url
 from . import views
+from django.contrib.auth import views as authViews
 
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^accounts/login/$', views.login, name='login')
+    url(r'^accounts/login/$', authViews.login, name='login')
 ]
