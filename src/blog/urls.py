26b6148975f36626f42a4b260f1f0f9c03622eb9
Aquaerius SSL <33044201+aquaerius@users.noreply.github.com>
2018-05-08 11:07:08 +0200

    Update urls.py

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     url(r'^post/(?P<pk>[0-9]+)/publish/$', views.post_publish, name='post_publish'),
     url(r'^post/(?P<pk>[0-9]+)/remove/$', views.post_remove, name='post_remove'),
     url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
+    url(r'^post/(?P<pk>\d+)/comment/remove/$', views.comment_remove, name='remove_comment_from_post'),
 ]
