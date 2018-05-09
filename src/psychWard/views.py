f8a0cf9440df77cdba4e6c149c7cdc7c10ce9909
Aquaerius SSL <33044201+aquaerius@users.noreply.github.com>
2018-05-09 11:02:37 +0200

    Update views.py

--- src/psychWard/views.py
+++ src/psychWard/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'psychward/index.html', {})
+    return render(request, 'psychward/index.html', {'title':'Psych Ward'})
 
 
 
