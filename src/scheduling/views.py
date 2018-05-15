5c84968708f365c842d2de8515b3105b881b8fb2
aquaerius <aquaerius@24dcaea7d349>
2018-05-15 08:22:06 +0000

    updated scheduling views

--- src/scheduling/views.py
+++ src/scheduling/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+
 
 # Create your views here.
+def service_list(request):
+    return render(request, 'scheduling/service_list.html', {})
+
+
+def owners_detail(request):
+    return render(request, 'scheduling/owners_detail.html', {})
+
+
+def contractors_detail(request):
+    return render(request, 'scheduling/contractors_detail.html', {})
+
+
+
