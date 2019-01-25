83015f51e0f38da66efebfa4e189ad06e0ac7b64
Aquaerius SSL <33044201+aquaerius@users.noreply.github.com>
2019-01-25 18:04:12 +0100

    Revert "Revert "Pull dev to master""

--- src/home/views.py
+++ src/home/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 #Defining the index view for home app
 def index(request):
-  return render(request, 'home/index.html', {})
+  return render(request, 'home/index.html', {'title':'Vesca - Planning, Scheduling &amp; Engineering'})
